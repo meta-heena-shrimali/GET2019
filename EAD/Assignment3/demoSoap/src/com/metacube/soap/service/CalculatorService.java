44b86c8e41faec97d2022ac8b32f8eeed992eaef
meta-heena-shrimali <heena.shrimali@metacube.com>
2019-02-20 14:37:50 +0530

    Assignment3

---
+++ EAD/Assignment3/demoSoap/src/com/metacube/soap/service/CalculatorService.java
@@ -0,0 +1,10 @@
+package com.metacube.soap.service;
+
+public class CalculatorService {
+
+	public double getArea(double side1, double side2, double side3){
+		double s=(side1+side2+side3)/2;
+		double area=Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
+		return area;
+	}
+}
